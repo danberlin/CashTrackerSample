bb488f1737c534f5578ccfdcf8c26cb8d1061d67
Dan Berlin <daniel.l.berlin@gmail.com>
2014-11-07 00:10:11 -0500

    Added new user account feature spec

---
+++ CashTracker/Automation/AddUserAccountSteps.cs
@@ -0,0 +1,44 @@
+﻿using TechTalk.SpecFlow;
+
+namespace CashTracker.Automation
+{
+    [Binding]
+    public class AddUserAccountSteps
+    {
+        [Given(@"there are no user accounts in the tracker")]
+        public void GivenThereAreNoUserAccountsInTheTracker()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"there is a user account ""(.*)""")]
+        public void GivenThereIsAUserAccount(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"user account ""(.*)"" has \$(.*)")]
+        public void GivenUserAccountHas(string p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I add the user account ""(.*)""")]
+        public void WhenIAddTheUserAccount(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I set the active user to ""(.*)""")]
+        public void WhenISetTheActiveUserTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I can set the active user to ""(.*)""")]
+        public void ThenICanSetTheActiveUserTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ CashTracker/Features/AddUserAccount.feature
@@ -0,0 +1,21 @@
+﻿Feature: Add User Account
+	In order to keep separate cash accounts for users
+	As a store owner
+	I want to be able to add a new user account
+
+Scenario: Add a new account
+	Given there are no user accounts in the tracker
+	When I add the user account "New User"
+	Then I can set the active user to "New User"
+
+Scenario: Separate balances
+	Given there is a user account "User1"
+	And user account "User1" has $0
+	And there is a user account "User2"
+	And user account "User2" has $0
+	When I set the active user to "User1"
+	And I deposit $100.00 into the tracker
+	And I set the active user to "User2"
+	Then I should have $0 in cash
+	When I set the active user to "User1"
+	Then I should have $100.00 in cash
